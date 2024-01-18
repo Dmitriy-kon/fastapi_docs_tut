bf900d78c2f8f0e18fbec5a91cf57bbdb5836c98
Konovalov Dmitriy <dkonowalow@gmail.com>
2024-01-18 15:15:01 +0600

    add Fields

--- src/app/models/item.py
+++ src/app/models/item.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(
+        gt=0,
+        description="цена должна быть больше нуля",
+    )
     tax: float | None = None
