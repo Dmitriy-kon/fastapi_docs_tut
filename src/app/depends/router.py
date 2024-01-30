51d0b9169a998b69bd742cbf0d3faa7057cf52d4
Konovalov Dmitriy <dkonowalow@gmail.com>
2024-01-30 14:49:23 +0600

    add pseudo type

--- src/app/depends/router.py
+++ src/app/depends/router.py
@@ -5,15 +5,17 @@ from fastapi import APIRouter, Depends
 router = APIRouter(tags=["depends"])
 
 
-async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+async def common_parameters(q: str = None, skip: int = 0, limit: int = 100):
     return {"q": q, "skip": skip, "limit": limit}
 
 
+CommonDep = Annotated[dict, Depends(common_parameters)]
+
 @router.get("/common-items")
-async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
+async def read_items(commons: CommonDep):
     return commons
 
 
 @router.get("/common-users")
-async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+async def read_users(commons: CommonDep):
     return commons
