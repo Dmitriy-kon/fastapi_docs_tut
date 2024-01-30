72dc21d9f979373a5846c7e5cfb929ed0ae3b12c
Konovalov Dmitriy <dkonowalow@gmail.com>
2024-01-30 18:51:24 +0600

    add class depends

--- src/app/depends/router.py
+++ src/app/depends/router.py
@@ -4,6 +4,15 @@ from fastapi import APIRouter, Depends
 
 router = APIRouter(tags=["depends"])
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: str = None, skip: int = 0, limit: int = 100) -> None:
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+        
 
 async def common_parameters(q: str = None, skip: int = 0, limit: int = 100):
     return {"q": q, "skip": skip, "limit": limit}
@@ -12,8 +21,13 @@ async def common_parameters(q: str = None, skip: int = 0, limit: int = 100):
 CommonDep = Annotated[dict, Depends(common_parameters)]
 
 @router.get("/common-items")
-async def read_items(commons: CommonDep):
-    return commons
+async def read_items_class(commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)]):
+    response = {}
+    if commons.q:
+        response |= {"q": commons.q}
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response |= {"items": items}
+    return response
 
 
 @router.get("/common-users")
